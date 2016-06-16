1d84af6dfa56d8651281e022dab165199e0a21ec
Peter Boers <pboers@desktop-01.students.os3.nl>
2016-06-16 14:06:14 +0200

    fix

--- chord.py
+++ chord.py
@@ -13,38 +13,18 @@ class ChordNode():
     def join(self):
         if self.address == self.hostip:
             print "Only node in group"
-            try:    
-                pid = os.fork()
+            try:
+                print "Starting server"
+                dht = DHT(self.hostip, self.port)
+                return dht
             except Exception, e:
                 raise e
 
-            if (pid == 0):
-                try:
-                    print "Starting server"
-                    dht = DHT(self.hostip, self.port)
-                    return dht
-                except Exception, e:
-                    raise e
-                finally:
-                    os._exit(0)
-            else:
-                pass
         else:
             print "Joining DHT group"
-            try:    
-                pid = os.fork()
+            try:
+                print "Starting server"
+                dht = DHT(self.hostip, self.port, boot_host=self.address, boot_port=self.port)
+                return dht
             except Exception, e:
                 raise e
-
-            if (pid == 0):
-                try:
-                    print "Starting server"
-                    dht = DHT(self.hostip, self.port, boot_host=self.address, boot_port=self.port)
-                    return dht
-                except Exception, e:
-                    raise e
-                finally:
-                    os._exit(0)
-            else:
-                pass
-    
