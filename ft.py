0f7bfae3d4cd4244f3b7226365b91c4832a2fea6
Peter Boers <peter@peterboers.info>
2016-06-17 15:16:08 +0200

    Its working?

--- ft.py
+++ ft.py
@@ -128,42 +128,8 @@ class Filter(object):
         print "Ip Header:", iph
         d_addr = socket.inet_aton(dest)
 
-        print "Old packet TCP Header", [d_addr, s_addr, source_port, sequence, acknowledgement, flags]
         newiph = pack('!BBHHHBBH4s4s', iph[0], iph[1], iph[2], iph[3], iph[4], iph[5], iph[6],iph[7],iph[8],d_addr)
 
-        # Lets check if the new packet is as good as we think it is....
         packet = iph + tcph + data
-
-
-        ip_header = packet[0:20]
-        iph = unpack('!BBHHHBBH4s4s', ip_header)
-        
-        # Variables to make it repackable
-        version_ihl = iph[0]
-        version = version_ihl >> 4
-        ihl = version_ihl & 0xF
-         
-        iph_length = ihl * 4
-
-        ttl = iph[5]
-        protocol = iph[6]
-        # Tcp stuff We Need this
-        tcp_header = packet[iph_length:iph_length+20]
-     
-        #now unpack them :)
-        tcph = unpack('!HHLLBBHHH' , tcp_header) 
-        source_port = tcph[0]
-        dest_port = tcph[1]
-        sequence = tcph[2]
-        acknowledgement = tcph[3]
-        doff_reserved = tcph[4]
-        tcph_length = doff_reserved >> 4
-        
-        flags = tcph[5]
-
-        s_addr = socket.inet_ntoa(iph[8])
-        d_addr = socket.inet_ntoa(iph[9])
-
-        print "New packet TCP Header", [d_addr, s_addr, source_port, sequence, acknowledgement, flags]
 
         return packet
