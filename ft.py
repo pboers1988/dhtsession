cd5a377df8eb5d514e2ea3c7bce7dd2cc3aad5e6
Peter Boers <peter@peterboers.info>
2016-06-17 14:35:40 +0200

    Sanity checking

--- ft.py
+++ ft.py
@@ -104,4 +104,34 @@ class Filter(object):
         newiph = pack('!BBHHHBBH4s4s', iph[0], iph[1], iph[2], iph[3], iph[4], iph[5], iph[6],iph[7],iph[8],d_addr)
         packet = newiph + buff[21:]
         print "Packet",repr(packet)
+
+        # Lets check if the new packet is as good as we think it is....
+        ip_header = packet[0:20]
+        iph = unpack('!BBHHHBBH4s4s', ip_header)
+        version_ihl = iph[0]
+        version = version_ihl >> 4
+        ihl = version_ihl & 0xF
+         
+        iph_length = ihl * 4
+
+        ttl = iph[5]
+        protocol = iph[6]
+        s_addr = socket.inet_ntoa(iph[8])
+        d_addr = socket.inet_ntoa(iph[9])
+        tcp_header = packet[iph_length:iph_length+20]
+        tcph = unpack('!HHLLBBHHH' , tcp_header) 
+        source_port = tcph[0]
+        dest_port = tcph[1]
+        sequence = tcph[2]
+        acknowledgement = tcph[3]
+        doff_reserved = tcph[4]
+        tcph_length = doff_reserved >> 4
+        
+        flags = tcph[5]
+
+        h_size = iph_length + tcph_length * 4
+        data_size = len(buff) - h_size
+
+        print [s_addr, source_port, sequence, acknowledgement, flags]
+
         return packet
