58bd3d8fd3d2ff3bb517ae80181e38f037cf70a9
Peter Boers <peter@peterboers.info>
2016-06-17 14:49:36 +0200

    Testing'

--- ft.py
+++ ft.py
@@ -92,22 +92,10 @@ class Filter(object):
     def repack(buff, dest):
         print "Repacking....."
         bufflength = len(buff)
-        print "Packet Length:", bufflength
         ip_header = buff[0:20]
         iph = unpack('!BBHHHBBH4s4s', ip_header)
-        print "Ip Header:", iph
-        print "Old dest addrs:", repr(iph[9])
-        print "Old dest addrs Human readable:", socket.inet_ntoa(iph[9])
-        print "The new Dest human readable:", dest
-        d_addr = socket.inet_aton(dest)
-        print "Packed dest address", repr(d_addr)
-        newiph = pack('!BBHHHBBH4s4s', iph[0], iph[1], iph[2], iph[3], iph[4], iph[5], iph[6],iph[7],iph[8],d_addr)
-        packet = newiph + buff[21:]
-        print "Packet",repr(packet)
-
-        # Lets check if the new packet is as good as we think it is....
-        ip_header = packet[0:20]
-        iph = unpack('!BBHHHBBH4s4s', ip_header)
+        
+        # Variables to make it repackable
         version_ihl = iph[0]
         version = version_ihl >> 4
         ihl = version_ihl & 0xF
@@ -116,9 +104,10 @@ class Filter(object):
 
         ttl = iph[5]
         protocol = iph[6]
-        s_addr = socket.inet_ntoa(iph[8])
-        d_addr = socket.inet_ntoa(iph[9])
-        tcp_header = packet[iph_length:iph_length+20]
+        # Tcp stuff We Need this
+        tcp_header = buff[iph_length:iph_length+20]
+     
+        #now unpack them :)
         tcph = unpack('!HHLLBBHHH' , tcp_header) 
         source_port = tcph[0]
         dest_port = tcph[1]
@@ -131,7 +120,46 @@ class Filter(object):
 
         h_size = iph_length + tcph_length * 4
         data_size = len(buff) - h_size
+        data = packet[h_size:]
 
-        print "New packet TCP Header", [s_addr, source_port, sequence, acknowledgement, flags]
+
+        print "Ip Header:", iph
+        d_addr = socket.inet_aton(dest)
+
+        print "Old packet TCP Header", [d_addr, s_addr, source_port, sequence, acknowledgement, flags]
+        newiph = pack('!BBHHHBBH4s4s', iph[0], iph[1], iph[2], iph[3], iph[4], iph[5], iph[6],iph[7],iph[8],d_addr)
+
+        # Lets check if the new packet is as good as we think it is....
+        packet = iph + tcph + data
+
+
+        ip_header = packet[0:20]
+        iph = unpack('!BBHHHBBH4s4s', ip_header)
+        
+        # Variables to make it repackable
+        version_ihl = iph[0]
+        version = version_ihl >> 4
+        ihl = version_ihl & 0xF
+         
+        iph_length = ihl * 4
+
+        ttl = iph[5]
+        protocol = iph[6]
+        # Tcp stuff We Need this
+        tcp_header = packet[iph_length:iph_length+20]
+     
+        #now unpack them :)
+        tcph = unpack('!HHLLBBHHH' , tcp_header) 
+        source_port = tcph[0]
+        dest_port = tcph[1]
+        sequence = tcph[2]
+        acknowledgement = tcph[3]
+        doff_reserved = tcph[4]
+        tcph_length = doff_reserved >> 4
+        
+        flags = tcph[5]
+
+
+        print "New packet TCP Header", [d_addr, s_addr, source_port, sequence, acknowledgement, flags]
 
         return packet
